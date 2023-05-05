5931a9820da6db8e57c7631e60cbba3cb13c9525
Léa Buguinet <lea@agencetaurine.fr>
2023-05-05 10:26:48 +0200

    init

---
+++ head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>AJ</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="dist/css/app.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@300;400&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"> 
+    </head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,42 @@
+
+
+<?php 
+    include('head.php');
+?>
+
+    <header>
+    <svg xmlns="http://www.w3.org/2000/svg" id="Calque_2" class="verriere t" viewBox="0 0 373.71 507.65"><defs><style>.cls-1{fill:none;stroke:#1a301b;stroke-miterlimit:10;stroke-width:2.5px}</style></defs><g id="Calque_1-2"><path d="M94.31 502.85V127.08" class="cls-1 ligneleft"/><path d="M281.96 507.65V127.46M239.23 138.52c0-27.72-22.76-50.19-50.83-50.19s-50.83 22.47-50.83 50.19h101.67Z" class="cls-1"/><path d="M281.99 128.86c0-51.31-41.62-83.26-93.59-83.26s-91.33 35.9-94.09 81.48" class="cls-1"/><path d="M209.69 138.52c0-11.61-9.53-21.02-21.29-21.02s-21.29 9.41-21.29 21.02h42.58ZM188.4 138.52l184.72-98.27M188.4 138.52 277.47.98M187.29 136.8 99.14.68M188.4 138.52.59 38M188.4 1.64v136.88M137.57 138.52l-136.43.04M238.73 138.56l133.74 1.1" class="cls-1"/></g></svg>
+
+    <img class="palmier" src="palmier.svg" alt="" /> 
+    <img class="plantgodown" src="plantgodown.svg" alt="" /> 
+    <img class="leafdown" src="leafdown.svg" alt="" /> 
+    <img class="orangeflower" src="orangeflower.svg" alt="" /> 
+    <img class="leafdownright" src="leafdownright.svg" alt="" /> 
+    <img class="whiteflowerleaf" src="whiteflowerleaf.svg" alt="" /> 
+    <img class="climbingplant" src="climbingplant.svg" alt="" />
+
+    <div>
+        <img class="logo" src="logo-aj.svg" alt="" />   
+     
+    </div>
+    
+    <a href='#menu'><span class="arrow"><img src="down-chevron.svg" alt="" />   </span></a>
+
+    
+    </header>
+
+    <main>
+
+        <nav id="menu">
+            <a href="#">ITEM 1</a>
+            <a href="#">ITEM 2</a>
+            <a href="#">ITEM 3</a>
+        </nav>
+
+    </main>
+
+
+<?php 
+    include('footer.php');
+
+?>
