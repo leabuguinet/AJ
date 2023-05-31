f20f17898e8d676ad5ba7dd6aef7afb67d5b8cec
Léa Buguinet <lea@agencetaurine.fr>
2023-05-31 18:09:44 +0200

    visit page wip

---
+++ visiter.php
@@ -0,0 +1,80 @@
+
+
+<?php 
+    include('head.php');
+?>
+
+
+
+    <main class="visiter pagewmenu">
+
+
+        <div class="header">
+            <div class="burger-container">
+            <div id="burger">
+                <div class="bar topBar"></div>
+                <div class="bar btmBar"></div>
+            </div>
+            </div>
+            <ul class="menu">
+            <li class="menu-item"><a href="/AJ/programme.php">Programme</a></li>
+            <li class="menu-item"><a href="/AJ/hebergement.php">Hébergement</a></li>
+            <li class="menu-item"><a href="#">À visiter</a></li>
+            </ul>
+
+        </div>
+
+        <div class="content">
+
+            <h1>Visiter les alentours</h1>
+
+            <h2>Argenton-sur-Creuse</h2>
+
+            <div class="visit-box">
+                <h3>• Centre ville</h3>
+                <p>Promenez-vous dans les ruelles de la "Venise du Berry" et admirez les maisons à galeries et à balustrades qui se reflètent dans la Creuse depuis le vieux pont.</p>
+                
+            </div>
+
+            <div class="visit-box">
+                <h3>• La Bonne Dame</h3>
+                <p>Elle domine depuis les hauteurs de la ville, perchée sur la Chapelle de la Bonne-Dame. Vous pourrez donc l'apercevoir depuis le centre ville mais il est encore plus intéressant de s'y rendre pour accéder à une superbe vue plongeante sur les alentours.  </p>
+                
+            </div>
+
+            <div class="visit-box">
+                <h3>• Le musée de la Chemiserie et de l'Élégance Masculine</h3>
+                <p>Installé dans le premier atelier de lingerie ouvert en 1860, le musée permet de découvrir le travail des "chemisières", et à travers celui-ci, l'histoire du vêtement masculin.</p>
+                
+            </div>
+
+
+            <h2>Saint-Marcel</h2>
+
+            <div class="visit-box">
+                <h3>• Le musée et le site archéologique Argentomagus</h3>
+                <p>Le musée, construit sur le site antique d'Argentomagus, propose un parcours qui retrace l'histoire de la cité gallo-romaine. Au programme, de nombreuses reconstitutions, des animations audiovisuelles, une crypte archéologique, la visite des vestiges et une balade dans un jardin romain et ses nombreuses plantes aromatiques.</p>
+            </div>
+            
+            <h2>Autres</h2>
+            
+            <div class="visit-box">
+                <h3>• La maison de George Sand</h3>
+                <p>Entrez dans l'intimité d'une grande écrivaine du XIXème siècle, George Sand. Visitez la maison, et le parc de Nohant, endroits indissociables de la vie et de l’œuvre de George Sand. Vous pourrez ainsi marcher dans les pas de l’écrivaine et de ses illustres invités ! </p>
+            </div>
+
+            <div class="visit-box">
+                <h3>• Le château de Sarzay</h3>
+                <p>Visitez l'impressionnante forteresse médiévale du XIVe siècle, qui est sortie intacte de la guerre de cent ans, des guerres de religions, de la Fronde et de la Révolution. Elle est aujourd'hui un monument historique classé.</p>
+            </div>
+
+        </div>
+
+
+    </main>
+
+
+<?php 
+    include('footer.php');
+
+?>
